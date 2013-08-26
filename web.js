a2a7fc9bd5d8926e06a2921fad93e0c60988760a
Ubuntu <ubuntu@ip-172-31-31-245.us-west-2.compute.internal>
2013-08-26 10:21:22 +0000

    Cambios en index.html y web version2

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs = require('fs');
-var htmlfile = "index.html";
+var htmlfile = "index2.html";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
